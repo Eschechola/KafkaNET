bf3d7be7d2e60889ee19fbd01f603ecaa864a69a
Eschechola <lucas.eschechola@outlook.com>
2021-04-18 04:14:29 -0300

    Adding project

---
+++ KafkaNET.Publisher/Program.cs
@@ -0,0 +1,48 @@
+﻿using Confluent.Kafka;
+using System;
+using System.Threading.Tasks;
+
+namespace KafkaNET.Publisher
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            string bootstrapServers = "localhost:9092";
+            string topicName = "log-events";
+
+            while (true)
+            {
+                Console.Write("Digite a mensagem: \n> ");
+                string message = Console.ReadLine();
+
+                try
+                {
+                    var config = new ProducerConfig
+                    {
+                        BootstrapServers = bootstrapServers
+                    };
+
+                    using (var producer = new ProducerBuilder<Null, string>(config).Build())
+                    {
+                        var result = await producer.ProduceAsync(
+                            topicName,
+                            new Message<Null, string>
+                            {
+                                Value = message
+                            }
+                        );
+
+                        Console.WriteLine("Mensagem: {0}\nStatus: {1}", message, result.Status.ToString());
+                    }
+
+                    Console.WriteLine("Concluído o envio de mensagens");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+    }
+}
---
+++ KafkaNET.Subscriber/Program.cs
@@ -0,0 +1,48 @@
+﻿using Confluent.Kafka;
+using System;
+using System.Threading;
+
+namespace KafkaNET.Subscriber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = new ConsumerConfig
+            {
+                BootstrapServers = "localhost:9092",
+                GroupId = "1",
+                AutoOffsetReset = AutoOffsetReset.Earliest
+            };
+
+            CancellationTokenSource cts = new CancellationTokenSource();
+
+            try
+            {
+                using (var consumer = new ConsumerBuilder<Ignore, string>(config).Build())
+                {
+                    consumer.Subscribe("log-events");
+
+                    try
+                    {
+                        while (true)
+                        {
+                            var cr = consumer.Consume(cts.Token);
+                            Console.WriteLine(
+                                $"Mensagem lida: {cr.Message.Value}");
+                        }
+                    }
+                    catch (OperationCanceledException)
+                    {
+                        consumer.Close();
+                        Console.WriteLine("Cancelada a execução do Consumer...");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
